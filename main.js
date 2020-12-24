81492eaedcf0121b64b187af85a04eb01c3953f2
Martin4124 <m.ibantschitz@gmail.com>
2020-12-24 13:57:57 +0100

    Desktop finished

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const Share = document.querySelector("aside")
 const Share_add = document.querySelector("#Share-add")
 const Share_remove = document.querySelector("#Share-remove")
 const footer = document.querySelector("footer")
-const x = window.matchMedia("(min-width: 700px)")
+const x = window.matchMedia("(min-width: 1000px)")
 
 Share_add.addEventListener("click", share1);
 Share_remove.addEventListener("click", share2);
 x.addListener(media);
 function share1(e){
     if(x.matches){
-        
+        Share.classList.toggle("flex");
+
+        Share.classList.add("animation-appear")
+        setTimeout(function(){
+            Share.classList.remove("animation-appear")
+        }, 2100) 
     }
     else{
         e.preventDefault();
@@ -20,12 +25,12 @@ function share1(e){
         Share.classList.add("animation-in")
         setTimeout(function(){
             Share.classList.remove("animation-in")
-        }, 1100)
+        }, 2100)
     }
 }
 function share2(e){
     if(x.matches){
-        
+        Share.classList.remove("flex");
     }
     else{
         e.preventDefault();
@@ -36,17 +41,15 @@ function share2(e){
         footer.classList.add("animation-in")
         setTimeout(function(){
             footer.classList.remove("animation-in")
-        }, 1100) 
+        }, 2100) 
     }
 }
 function media(x){
-    if(footer.classList.contains("hidden")){
     footer.classList.remove("hidden");
     Share.classList.remove("flex");
 
     footer.classList.add("animation-in")
         setTimeout(function(){
             footer.classList.remove("animation-in")
-        }, 1100) 
-    }
+        }, 2100) 
 }
